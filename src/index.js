7759e7aaff1afef204a398f48dd40581afa574a1
Kanchan <kanchan96437@gmail.com>
2025-05-02 20:45:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import GreetingComponent from './GreetingComponent';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function App() {
-  return (
-    <div className="App">
-      <GreetingComponent />
-    </div>
-  );
-}
 
-export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
